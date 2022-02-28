b311057b3085cae162a4f631d29226af17877e98
DaxKLop 🤡 <daxklop@mail.ru>
2022-02-28 22:04:51 +0300

    Конец

--- Лаба 11/Лаба 11.cpp
+++ Лаба 11/Лаба 11.cpp
@@ -1,8 +1,11 @@
 ﻿#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 #include <locale.h>
+#include <conio.h>
 #include <string.h>
 #include <Windows.h>
+using namespace std;
 
 typedef struct 
 {
@@ -10,9 +13,9 @@ typedef struct
 	char initials[30];
 	char birthday[30];
 	char receipt_date[30];
-	float math_score;
-	float physics_grade;
-	float computer_science_assessment;
+	int math_mark;
+	int physics_mark;
+	int computer_science_mark;
 } student_t;
 
 int menu();
@@ -71,43 +74,109 @@ int menu()
 }
 
 
+void enterOnlyLetterString(char* s)
+{
+	int i = 0;
+	while (1)
+	{
+		s[i] = _getch();
+		if (s[i] == 13) break;
+		if (((((int)s[i] >= 65 && (int)s[i] <= 90) || ((int)s[i] >= 97 && (int)s[i] <= 122) || ((int)s[i] <= -1 && (int)s[i] >= -64))))
+		{
+			cout << s[i];
+			i++;
+		}
+	}
+	s[i] = '\0';
+}
+
+void enterInitials(char* s)
+{
+	int i = 0;
+	while (1)
+	{
+		s[i] = _getch();
+		if (s[i] == 13) break;
+		if (((((int)s[i] >= 65 && (int)s[i] <= 90) || ((int)s[i] <= -23 && (int)s[i] >= -64) || s[i] == '.')))
+		{
+			cout << s[i];
+			i++;
+		}
+	}
+	s[i] = '\0';
+}
+
+void enterDate(char* s)
+{
+	int i = 0;
+	while (1)
+	{
+		s[i] = _getch();
+		if (s[i] == 13) break;
+		if (((int)s[i] >= 48 && (int)s[i] <= 57) || s[i] == '.')
+		{
+			cout << s[i];
+			i++;
+		}
+	}
+	s[i] = '\0';
+}
+
+void enterMarks(char* s)
+{
+	int i = 0;
+	while (1)
+	{
+		s[i] = _getch();
+		if (s[i] == 13) break;
+		if (((int)s[i] >= 48 && (int)s[i] <= 57) || s[i] == ',')
+		{
+			cout << s[i];
+			i++;
+		}
+	}
+	s[i] = '\0';
+}
+
 void enterStudent(student_t* student)
 {
 	printf("Введите фамилию:\n");
-	scanf_s("%s", student->surname,sizeof(student->surname));
-	printf("Введите инициалы:\n");
-	scanf_s("%s", student->initials,sizeof(student->initials));
-	printf("Введите день рождения (в формате 	DD.MM.YYYY) :\n");
-	scanf_s("%s", student->birthday,sizeof(student->birthday));
-	printf("Введите дату поступлени (в формате 	DD.MM.YYYY) :\n");
-	scanf_s("%s", student->receipt_date,sizeof(student->receipt_date));
-	printf("Введите оценку по математике:\n");
-	scanf_s("%f", &student->math_score,sizeof(student->math_score));
+	enterOnlyLetterString(student->surname);
+	printf("\nВведите инициалы:\n");
+	enterInitials(student->initials);
+	printf("\nВведите день рождения (в формате 	DD.MM.YYYY) :\n");
+	enterDate(student->birthday);
+	printf("\nВведите дату поступлени (в формате 	DD.MM.YYYY) :\n");
+	enterDate(student->receipt_date);
+	printf("\nВведите оценку по математике:\n");
+	scanf_s("%d", &student->math_mark,sizeof(student->math_mark));
+	fseek(stdin, 0, SEEK_END);
 	printf("Введите оценку по физике:\n");
-	scanf_s("%f", &student->physics_grade,sizeof(student->physics_grade));
+	scanf_s("%d", &student->physics_mark,sizeof(student->physics_mark));
+	fseek(stdin, 0, SEEK_END);
 	printf("Введите оценку по информатике:\n");
-	scanf_s("%f", &student->computer_science_assessment,sizeof(student->computer_science_assessment));
+	scanf_s("%d", &student->computer_science_mark,sizeof(student->computer_science_mark));
+	fseek(stdin, 0, SEEK_END);
 }
 
-void printStudent(student_t* student, int *flag)
+void printStudent(student_t* student)
 {
-	if (*flag == 1)
-	{
+	
 	printf("Фамилия : %s\n", student->surname);
 	printf("Инициалы : %s\n", student->initials);
 	printf("День рождения : %s\n", student->birthday);
 	printf("Дата поступления : %s\n", student->receipt_date);
-	printf("Оценка по математике : %.2f\n", student->math_score);
-	printf("Оценка по физике : %.2f\n", student->physics_grade);
-	printf("Оценка по физике : %.2f\n", student->computer_science_assessment);
+	printf("Оценка по математике : %d\n", student->math_mark);
+	printf("Оценка по физике : %d\n", student->physics_mark);
+	printf("Оценка по информатике : %d\n", student->computer_science_mark);
 	printf("____________\n");
-	}
-	else if (*flag == 2)
-	{
-		printf("Фамилия : %s\n", student->surname);
-		printf("Инициалы : %s\n", student->initials);
-		printf("День рождения : %s\n", student->birthday);
-	}
+}
+
+void printSomeAttributes(student_t* student)
+{
+	printf("Фамилия : %s\n", student->surname);
+	printf("Инициалы : %s\n", student->initials);
+	printf("День рождения : %s\n", student->birthday);
 }
 
 void form()
@@ -197,13 +266,13 @@ bool readStudents(student_t** students, int *sch)
 void printAllStudents()
 {
 	student_t* students = NULL;
-	int sch, flag = 1;
+	int sch;
 	if (!readStudents(&students, &sch)) 
 		return;
 		
 	for (int i = 0; i < sch; i++)
 	{
-		printStudent(&students[i], &flag);
+		printStudent(&students[i]);
 	}
 	free(students);
 }
@@ -211,7 +280,7 @@ void printAllStudents()
 void searchStudentBySurname()
 {
 	student_t* students = NULL;
-	int sch, flag = 2;
+	int sch;
 	if (!readStudents(&students, &sch))
 		return;
 	if (sch == 0)
@@ -231,7 +300,7 @@ void searchStudentBySurname()
 	{
 		if (strcmp(students[i].surname, se) == 0)
 		{
-			printStudent(&students[i], &flag);
+			printStudent(&students[i]);
 			isFound = true;
 		}
 	}
@@ -245,7 +314,7 @@ void searchStudentBySurname()
 void printStudentWithNameStartWithA()
 {
 	student_t* students = NULL;
-	int sch, flag = 2;
+	int sch;
 	char str2[2] = "А";
 	char *istr[100];
 	if (!readStudents(&students, &sch))
@@ -255,11 +324,11 @@ void printStudentWithNameStartWithA()
 	{
 		*istr = strstr(students[i].surname, str2);
 		if (*istr - students[i].surname + 1 == 1)
-		printStudent(&students[i], &flag);
+		printSomeAttributes(&students[i]);
 		char str2[2] = "A";
 		*istr = strstr(students[i].surname, str2);
 		if (*istr - students[i].surname + 1 == 1)
-			printStudent(&students[i], &flag);
+			printSomeAttributes(&students[i]);
 	}
 	free(students);
 }
